8346aa0e5a1bdfcb17fa63fca38b65eee36759f4
lydia <lydiastew01@gmail.com>
2026-01-16 02:12:31 +0000

    added content to pages

---
+++ src/supabase.js
@@ -0,0 +1,5 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+const supabaseUrl = 'https://ximisbmpcyvjxdutrpjb.supabase.co';
+const supabaseKey = 'sb_publishable_5A-pOeeAQgMfRBwN35AfKw_QLxg6j4I';
+export const supabase = createClient(supabaseUrl, supabaseKey);
